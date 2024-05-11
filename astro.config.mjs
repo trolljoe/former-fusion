1146971aa6952d7731eee175a38375e98851f2ca
joetroll <joetroll@proton.me>
2024-05-11 01:37:17 -0300

    change rss feed link

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-theme-cody.netlify.app',
+  site: 'https://blog.joetroll.is-a.dev',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
